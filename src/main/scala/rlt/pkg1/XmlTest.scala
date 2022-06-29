b57b57711188de6e4781e1ff6179fc84615ead39
rongcheng <li.rongcheng@gmail.com>
2022-06-30 02:06:53 +1000

    Update XmlTest.scala

--- src/main/scala/rlt/pkg1/XmlTest.scala
+++ src/main/scala/rlt/pkg1/XmlTest.scala
@@ -5,24 +5,29 @@ import scala.xml.PrettyPrinter
 object XmlTest {
 
   val name = "RL";
-  val dateString = java.text.DateFormat.getDateInstance().format(new java.util.Date())
+  val dateString: String = java.text.DateFormat.getDateInstance().format(new java.util.Date())
 
-  def printXmlTemplate {
+  def printXmlTemplate(): Unit = {
     val xml_string =
       <dateMsg addressedTo={name}>
-        <test>
-          Hello, {name}! Today is {dateString}
-        </test>
-        <test>
+        <test1>
+          <test2>
+            Hello, {name}! Today is {dateString}
+          </test2>
+          <test2>
+            Hello, {name}! Today is {dateString}
+          </test2>
+        </test1>
+        <test1>
           test
-        </test>
+        </test1>
       </dateMsg>;
 
-    var printer = new PrettyPrinter(80,8) // _note_: 80 is line width, 8 is indent space number
+    var printer = new PrettyPrinter(80,4) // _note_: 80 is line width, 4 is indent space number
     println(printer.format(xml_string))
   }
 
-  def main(args: Array[String]){
-    printXmlTemplate
+  def main(args: Array[String]): Unit = {
+    printXmlTemplate()
   }
 }
